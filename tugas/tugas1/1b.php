53903406a0b9aa87a743b779b672eef1f13f3fc7
IlmanHanifa <ilmanhanifa9@gmail.com>
2023-02-22 12:23:27 +0700

    menambahkan folder baru

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tugas 1b.php</title>
+</head>
+<body>
+<?php $npm = "65";?>
+<?php echo 'Aku adalah <b>'. $npm . '</b><br>';
+?>
+
+<?php echo 'Jika aku dikali 5, maka aku sekarang menjadi <b>'. $npm*5 . '</b><br>'
+ ?>
+<?php echo 'Jika aku dibagi 2, maka aku sekarang menjadi <b>'. $npm/2 . '</b><br>'
+ ?>
+<?php echo 'Jika aku ditambah 75, maka aku sekarang menjadi <b>'. $npm+75 . '</b><br>' 
+ ?>
+<?php echo 'Jika aku dikurang 20, maka aku sekarang menjadi <b>'. $npm-20 . '</b><br>'
+?>
+</body>
+</html>
---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    
+    <title>tugas1c.php</title>
+    <style>
+    .ilman-hanifa {
+            width: 500px;
+            border: 1px solid black;
+            margin:auto;
+            padding :10px;
+        }
+
+    .kotak1,.kotak2,.kotak3{
+        width: 100px;
+        height: 100px;
+        border: 1px solid black;
+        background-color: rgb(73, 234, 44);
+        color: black;
+        font-size: 28px;
+        text-align: center;
+        line-height: 100px;
+    }
+
+    .kotak2{
+        display: inline-block;
+    }
+    .kotak3{
+        display: inline-block;
+    }
+
+    </style>
+
+
+</head>
+<body>
+    <div class="ilman-hanifa">
+    
+     <div class="kotak1">1</div>
+
+
+<div class="kotak2">2</div><div class="kotak2">2</div>
+<br>
+<div class="kotak3">3</div><div class="kotak3">3</div><div class="kotak3">3</div>
+</div>
+
+</body>
+</html>
